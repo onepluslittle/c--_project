a3e701d30336e42c16fa6b3a89a6ccdeb52e90f1
onepluslittle <1184416869@qq.com>
2016-03-08 22:34:48 +0800

    huawei_v0.3
    simple path_data read and result.csv

--- c++_project/Graphmtx.h
+++ c++_project/Graphmtx.h
@@ -1,15 +1,13 @@
 // file : Graphmtx.h
 // description : graph as adjacency matrix and Dijkstra
-// 2016.3.7 
+//               read path_data to m_demandVertice[]
+// version : 0.3
+// 2016.3.8 22:22
 
 #ifndef HUAWEI_GRAPHMTX_H_VERSION1
 #define HUAWEI_GRAPHMTX_H_VERSION1
-
-//#include <cassert>
 #include <iostream>
-#include <vector>
 namespace haiwei_v1 {
-
 const int DefaultMaxVertices = 30;
 const int maxWeight = 100;
 class Graphmtx
@@ -22,9 +20,12 @@ public:
 	~Graphmtx(void);
 	int GetVertices(void) { return this->m_numOfVertices; }
 	bool insertEdge(int indexOfEdge, int begin, int end, int Weight);
+	bool insertDemandVertices(int index);
 	//void print(void);
 	std::ostream& print(std::ostream& out);
-	void Dijkstra(int begin, std::ostream& out);
+	//void Dijkstra(int begin, std::ostream& out);
+	void DijkstraPath(int begin, int end, std::ostream& out);
+	void DijkstraPath_minHeap(int begin, int end, std::ostream& out);
 
 private:
 	int m_maxOfVertices;
@@ -32,7 +33,15 @@ private:
 	int m_numOfEdges;
 	int* m_VerticesList;
 	int* m_EdgesList;
-	int** m_Edge;
+	struct  Edge {
+		int index;
+		int weight;
+		Edge(int i, int w): index(i), weight(w) {}
+		Edge() : index(-1), weight(0) {}
+	}; // Edge
+	Edge** m_Edge;
+	int* m_demandVertice;
+	int m_numOfDemandVertice;
 }; // class Graphmtx
 
 Graphmtx::Graphmtx(int maxOfVertices)
@@ -40,17 +49,20 @@ Graphmtx::Graphmtx(int maxOfVertices)
 	this->m_maxOfVertices = (0 < maxOfVertices && maxOfVertices <= 600) ? DefaultMaxVertices : maxOfVertices;
 	this->m_numOfVertices = this->m_maxOfVertices;
 	m_VerticesList = new int[m_maxOfVertices];
-	m_Edge = (int**) new int*[m_maxOfVertices];
+	m_Edge = (Edge**) new Edge*[m_maxOfVertices];
 	for (int i = 0; i < m_maxOfVertices; ++i) {
-		m_Edge[i] = new int[m_maxOfVertices];
+		m_Edge[i] = new Edge[m_maxOfVertices];
 		//initialize the matrix = 0 or maxWeight
 		for (int j = 0; j < m_maxOfVertices; ++j)
-			m_Edge[i][j] = (i == j)? 0 : maxWeight;
+			m_Edge[i][j].weight = (i == j)? 0 : maxWeight;
 	}
+	m_demandVertice = new int[m_maxOfVertices];
+	this->m_numOfDemandVertice = 0;
 }
 
 Graphmtx::~Graphmtx() {
 	delete [] m_VerticesList;
+	delete [] m_demandVertice;
 	for (int i = 0; i < m_maxOfVertices; ++i)
 		delete [] m_Edge[i];
 }
@@ -58,84 +70,88 @@ Graphmtx::~Graphmtx() {
 bool Graphmtx::insertEdge(int indexOfEdge, int begin, int end, int weight){
 	//only record the min Weight among the parallel Edge
 	if (-1 < begin && begin < m_maxOfVertices && -1 < end && end < m_maxOfVertices
-		&& 1 <= weight && weight <= 20 && weight < m_Edge[begin][end]) {
-		m_Edge[begin][end] = weight;
+		&& 1 <= weight && weight <= 20 && weight < m_Edge[begin][end].weight) {
+		m_Edge[begin][end].index = indexOfEdge;
+		m_Edge[begin][end].weight = weight;
 		++m_numOfEdges;
 		return true;
 	}
 	else return false;
 }
 
+bool Graphmtx::insertDemandVertices(int index) {
+	if (0 <= index && index < this->m_maxOfVertices) {
+		this->m_demandVertice[++m_numOfDemandVertice] = index;
+		return true;
+	}
+	return false;
+}
 std::ostream& Graphmtx::print(std::ostream& out) {
 	out << "vertices : " << m_numOfVertices << ", edges : " << m_numOfEdges << std::endl;
 	for (int i = 0; i < m_maxOfVertices; ++i)
 		for (int j = 0; j < m_maxOfVertices; ++j) {
-			int weight = m_Edge[i][j];
+			int weight = m_Edge[i][j].weight;
 			if (0 < weight && weight < maxWeight)
 				out << '(' << i << ',' << j << ',' << weight << ")\n";
 		}
 	return out;
 }
 
-
-// Dijkstra algorithm
-// parameters : 
-//     begin   : the start vertice
-//     dist[j] : the shortest path length for begin to j, 0 <= j < n
-//     path[j] : store the shortest path length, 0 <= j < n
-//void ShortestsPath(Graphmtx& G, int begin, int* dist, int* path) {
-//	const int n = G.m_numOfVertices;
+//void Graphmtx::Dijkstra(int begin, std::ostream& out) {
+//	const int n = this->m_numOfVertices;
+//	int* dist = new int[n];
+//	int* path = new int[n];
+//	//std::vector<int> path(n);
 //	bool* S = new bool[n];//the set of vertices in the shortest path
 //	int i, j, k;
 //	int weight, minWeight;
 //	for (i = 0; i < n; ++i) {
-//		dist[i] = G.m_Edge[begin][i]; // initialize  the dist[]
+//		dist[i] = this->m_Edge[begin][i].weight; // initialize  the dist[]
 //		S[i] = false;
-//		*(path+i) = (i != begin && dist[i] < maxWeight) ? begin : -1;
+//		path[i] = (i != begin && dist[i] < maxWeight) ? begin : -1;
 //	} // end for
 //	S[begin] = true; dist[begin] = 0;
 //	for (i = 0; i < n - 1; ++i) {
 //		minWeight = maxWeight; int u = begin;
 //		for (j = 0; j < n; ++j)
-//			if (S[j] == false && *(dist+j) < minWeight) {u = j; minWeight = *(dist+j);}
+//			if (i != j && S[j] == false && *(dist + j) < minWeight) { u = j; minWeight = *(dist + j); }
 //		S[u] = true;
 //		for (k = 0; k < n; ++k) {
-//			weight = G.m_Edge[u][k];
+//			weight = this->m_Edge[u][k].weight;
 //			if (S[k] == false && weight < maxWeight && dist[u] + weight < dist[k]) {
-//				*(dist+k) = *(dist+u) + weight;
-//				*(path+k) = u;
+//				*(dist + k) = *(dist + u) + weight;
+//				path[k] = u;
 //			}
 //		}// end inner for 
 //	}// end outer for
-//	delete []S;
-//}
-
-//void printShortestPath(std::ostream& out, Graphmtx& G, int begin, int* dist, int* path) {
+//	delete[]S;
+//
 //	out << "从起点" << begin << "到其他各顶点的最短路径为：" << std::endl;
-//	int i, j, k, n = G.m_numOfVertices;
 //	int* d = new int[n];
 //	for (i = 0; i < n; ++i) {
 //		if (i != begin) {
 //			j = i; k = 0;
-//			while (j != begin) { d[k++] = j; j = *(path+j); }
-//			out << "the shortest path of vertice " << i << "is " << begin;
-//			while (k > 0) { out << d[--k] << " "; }
-//			out << "the shortest path length is " << dist[i] << std::endl;
+//			while (j != begin && j != -1) { d[k++] = j; j = path[j]; }
+//			out << "the shortest path of vertice " << i << " is " << begin;
+//			//output the path
+//			while (k > 0) { out << "|" << d[--k] ; }
+//			out << ".The shortest path length is " << dist[i] << std::endl;
 //		}
 //	} // end for
-//	delete []d;
+//	delete[]d;
+//	//delete[]path;
+//	delete[]dist;
 //}
 
-void Graphmtx::Dijkstra(int begin, std::ostream& out) {
+void Graphmtx::DijkstraPath(int begin, int end, std::ostream& out) {
 	const int n = this->m_numOfVertices;
 	int* dist = new int[n];
 	int* path = new int[n];
-	//std::vector<int> path(n);
 	bool* S = new bool[n];//the set of vertices in the shortest path
 	int i, j, k;
 	int weight, minWeight;
 	for (i = 0; i < n; ++i) {
-		dist[i] = this->m_Edge[begin][i]; // initialize  the dist[]
+		dist[i] = this->m_Edge[begin][i].weight; // initialize  the dist[]
 		S[i] = false;
 		path[i] = (i != begin && dist[i] < maxWeight) ? begin : -1;
 	} // end for
@@ -146,30 +162,35 @@ void Graphmtx::Dijkstra(int begin, std::ostream& out) {
 			if (i != j && S[j] == false && *(dist + j) < minWeight) { u = j; minWeight = *(dist + j); }
 		S[u] = true;
 		for (k = 0; k < n; ++k) {
-			weight = this->m_Edge[u][k];
+			weight = this->m_Edge[u][k].weight;
 			if (S[k] == false && weight < maxWeight && dist[u] + weight < dist[k]) {
 				*(dist + k) = *(dist + u) + weight;
 				path[k] = u;
+				//if (k == end) break;
 			}
 		}// end inner for 
+		//if (k == end) break;
 	}// end outer for
 	delete[]S;
-
-	out << "从起点" << begin << "到其他各顶点的最短路径为：" << std::endl;
-	int* d = new int[n];
-	for (i = 0; i < n; ++i) {
-		if (i != begin) {
-			j = i; k = 0;
-			while (j != begin && j != -1) { d[k++] = j; j = path[j]; }
-			out << "the shortest path of vertice " << i << " is " << begin;
-			//output the path
-			while (k > 0) { out << "|" << d[--k] ; }
-			out << ".The shortest path length is " << dist[i] << std::endl;
-		}
-	} // end for
-	delete[]d;
-	//delete[]path;
 	delete[]dist;
+
+	// use stack to store the path from end->begin, reverse to output
+	int* stack = new int[n];
+	i = end; k = 0;
+	while (i != begin) {
+		stack[++k] = i;
+		i = path[i];
+	}
+	stack[++k] = begin;
+	while (k > 1) { out << (this->m_Edge[stack[k]][stack[k-1]]).index; --k;  if (k > 2) out << '|'; }
+	out << '\n';
+
+	delete[]stack;
+	delete[]path;
+}
+
+void Graphmtx::DijkstraPath_minHeap(int begin, int end, std::ostream& out) {
+
 }
 
 } // namespace haiwei_v1
--- c++_project/test.cpp
+++ c++_project/test.cpp
@@ -1,6 +1,7 @@
 #include "Graphmtx.h"
 #include <fstream>
 #include <ctime>
+//#include <stdlib.h>
 
 int main()
 {
@@ -9,11 +10,12 @@ int main()
 	haiwei_v1::Graphmtx G(600);
 	std::fstream graph_data("graph_data.csv", std::ios::in);
 	if (!graph_data) { std::cerr << "graph_data file could not be opened"; 	system("pause"); exit(1); }
-	char buffer[20] = "\n";
+	const int buf_length = 20;
+	char buffer[buf_length] = "\n";
 	int index, begin, end, weight;
 	time_t data_read_start = clock();
 	while (!graph_data.eof()) {
-		graph_data.getline(buffer, 20);
+		graph_data.getline(buffer, buf_length);
 		sscanf(buffer, "%d,%d,%d,%d", &index, &begin, &end, &weight);
 		G.insertEdge(index, begin, end, weight);
 	}
@@ -21,9 +23,16 @@ int main()
 
 	std::fstream path_data("path_data.csv", std::ios::in);
 	if (!path_data) { std::cerr << "path_data file could not be opened"; system("pause"); exit(1); }
-	std::string  verticesDemand;
-	path_data.getline(buffer, 20);
-	sscanf(buffer, "%d,%d,%s", &begin, &end, verticesDemand.c_str());
+	//std::string  verticesDemand;
+	char buffer2[100] = "\n";
+	path_data.getline(buffer2, sizeof(buffer2));
+	sscanf(buffer2, "%d,%d,%s", &begin, &end, buffer2);
+	char* token;
+	for (token = strtok(buffer2, "|"); NULL != token; token = strtok(NULL, "|")) {
+		int index = atoi(token);
+		G.insertDemandVertices(index);
+	}
+
 	path_data.close();
 	time_t data_read_end = clock();
 	//*************************************
@@ -32,13 +41,10 @@ int main()
 
 	//shortest_path_part ******************	
 	time_t path_find_start = clock();
-	//int* dist = new int[G.GetVertices()];
-	//int* path = new int[G.GetVertices()];
-	//haiwei_v1::ShortestsPath(G, begin, dist, path);
-	//haiwei_v1::printShortestPath(std::cout, G, begin, dist, path);
 	std::ofstream result;
 	result.open("result.csv");
-	G.Dijkstra(begin, result);
+	G.DijkstraPath(begin, end, result);
+	G.DijkstraPath(begin, end, std::cout);
 	time_t path_find_end = clock();
 	//haiwei_v1::printShortestPath(result, G, begin, dist, path);
 	result << "data_read : " << double(data_read_end - data_read_start) / CLOCKS_PER_SEC << std::endl;
@@ -46,17 +52,9 @@ int main()
 	result.close();
 	//**************************************
 
-	//delete []dist;
-	//delete []path;
-
 	time_t program_end = clock();
-
-	//show_cost_time_part
-	//std::cout << "data_read : " << double(data_read_end - data_read_start) / CLOCKS_PER_SEC << std::endl;
-	//result << "path_find_total_time : " << double(path_find_end - path_find_start) / CLOCKS_PER_SEC << std::endl;
-	//std::cout << "program_total_time : " << double(program_end - program_start) / CLOCKS_PER_SEC << std::endl;
 	//**************************************
-
+	std::cout << "done\n";
 	system("pause");
 	return 0;
 }
